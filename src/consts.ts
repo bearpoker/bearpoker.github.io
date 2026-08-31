fa3df7f85f3b1ff6591e1daeb266b54ca56a69dd
bearpoker <bearpoker123@gmail.com>
2026-08-31 20:26:04 +0800

    Initial commit

---
+++ src/consts.ts
@@ -0,0 +1,5 @@
+// Place any global data in this file.
+// You can import this data from anywhere in your site by using the `import` keyword.
+
+export const SITE_TITLE = '扑克熊的博客';
+export const SITE_DESCRIPTION = '记录学习与生活，分享技术心得';
