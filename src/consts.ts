17b58e2229cf4487576a97ff150e6463fbc5ebbd
bearpoker <bearpoker123@gmail.com>
2026-08-31 20:52:47 +0800

    博客内容调整

--- src/consts.ts
+++ src/consts.ts
@@ -2,4 +2,4 @@
 // You can import this data from anywhere in your site by using the `import` keyword.
 
 export const SITE_TITLE = '扑克熊的博客';
-export const SITE_DESCRIPTION = '记录学习与生活，分享技术心得';
+export const SITE_DESCRIPTION = '分享 AI 学习、实践与项目';
